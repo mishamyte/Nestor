cffebd10c06974c404ed4e4e99931f4adf53f185
Myte <myte@ukr.net>
2017-07-12 23:38:41 +0300

    Default Console Logger implemented

---
+++ Nestor/Interfaces/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Nestor.Interfaces
+{
+	public interface ILogger
+	{
+		void LogMessage(string message);
+		void LogError(string error);
+	}
+}
---
+++ Nestor/Utils/DefaultConsoleLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nestor.Interfaces;
+
+namespace Nestor.Utils
+{
+	public class DefaultConsoleLogger : ILogger
+	{
+		public void LogMessage(string message)
+		{
+			Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] {message}");
+		}
+
+		public void LogError(string error)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] ERROR: {error}");
+			Console.ResetColor();
+		}
+	}
+}
